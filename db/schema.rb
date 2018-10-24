a944b45e44c2cc7aa08b31491036b36acda89c0f
Jayanth Dungavath <jayanthdeejay@gmail.com>
2018-10-24 11:00:46 -0400

    Update to most recent release of Hyrax

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181001192124) do
+ActiveRecord::Schema.define(version: 20180809170955) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -543,8 +543,6 @@ ActiveRecord::Schema.define(version: 20181001192124) do
     t.binary "zotero_token"
     t.string "zotero_userid"
     t.string "preferred_locale"
-    t.string "first_name"
-    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
