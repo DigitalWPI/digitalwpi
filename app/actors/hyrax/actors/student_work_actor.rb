171152b8d1674927602c9a2207fd080d35259558
Jayanth Dungavath <jayanthdeejay@gmail.com>
2018-11-14 15:39:58 -0500

    Created Student Work type

---
+++ app/actors/hyrax/actors/student_work_actor.rb
@@ -0,0 +1,8 @@
+# Generated via
+#  `rails generate hyrax:work StudentWork`
+module Hyrax
+  module Actors
+    class StudentWorkActor < Hyrax::Actors::BaseActor
+    end
+  end
+end
--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -3,6 +3,8 @@ Hyrax.config do |config|
   config.register_curation_concern :etd
   # Injected via `rails g hyrax:work GenericWork`
   config.register_curation_concern :generic_work
+  # Injected via `rails g hyrax:work StudentWork`
+  config.register_curation_concern :student_work
   # Register roles that are expected by your implementation.
   # @see Hyrax::RoleRegistry for additional details.
   # @note there are magical roles as defined in Hyrax::RoleRegistry::MAGIC_ROLES
