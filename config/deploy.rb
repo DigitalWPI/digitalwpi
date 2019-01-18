0e360ed070916566720d429758194a5a873561b6
Alfred Scott <awscott@wpi.edu>
2019-01-18 08:53:58 -0500

    cap updates, todos added

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,6 +25,7 @@ set :deploy_to, "/home/webapp/#{my_app_name}"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
