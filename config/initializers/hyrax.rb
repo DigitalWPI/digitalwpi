18b2fa241659e1417a8118be5c2b3565491d1c76
Alfred Scott <awscott@wpi.edu>
2019-03-05 13:18:28 -0500

    pathname objects in hyrax.rb

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -191,7 +191,7 @@ Hyrax.config do |config|
   # Location on local file system where derivatives will be stored
   # If you use a multi-server architecture, this MUST be a shared volume
   # config.derivatives_path = Rails.root.join('tmp', 'derivatives')
-  config.derivatives_path = File.join(ENV["NFS_DIR"], "rails", "derivatives")
+  config.derivatives_path =  Pathname.new(ENV["NFS_DIR"]) + "rails" + "derivatives"
 
   # Should schema.org microdata be displayed?
   # config.display_microdata = true
@@ -204,7 +204,7 @@ Hyrax.config do |config|
   # prior to being ingested into the repository or having derivatives generated.
   # If you use a multi-server architecture, this MUST be a shared volume.
   # config.working_path = Rails.root.join( 'tmp', 'uploads')
-  config.working_path = File.join( ENV["NFS_DIR"], "rails" ,"uploads")
+  config.working_path = Pathname.new(ENV["NFS_DIR"]) + "rails" + "uploads"
   
   # Should the media display partial render a download link?
   # config.display_media_download_link = true
