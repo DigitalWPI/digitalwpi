6cb759430a1ba8afd6d054d52a2aa9b3ba48a632
Alfred Scott <awscott@wpi.edu>
2019-03-21 16:19:45 -0400

    more tweaks

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :deploy_to, "/var/#{my_app_name}"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-append :linked_dirs, "log", "vendor", "public/assets"
+append :linked_dirs, "log", "vendor", "public/assets", "tmp"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
