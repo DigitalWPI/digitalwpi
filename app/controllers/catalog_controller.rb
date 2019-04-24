32ec73a293391264f23e9c172b842fbd510d2121
Alfred Scott <awscott@wpi.edu>
2019-04-24 15:32:39 -0400

    added some minor tweaks and dep to unit

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -41,9 +41,10 @@ class CatalogController < ApplicationController
 
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
-    config.add_facet_field solr_name("human_readable_type", :facetable), label: "Type", limit: 5
+    # config.add_facet_field solr_name("human_readable_type", :facetable), label: "Type", limit: 5
     config.add_facet_field solr_name("resource_type", :facetable), label: "Resource Type", limit: 5
     config.add_facet_field solr_name("creator", :facetable), limit: 5
+    config.add_facet_field solr_name("advisor", :facetable), label: "Advisor", limit: 5
     config.add_facet_field solr_name("contributor", :facetable), label: "Contributor", limit: 5
     config.add_facet_field solr_name("keyword", :facetable), limit: 5
     config.add_facet_field solr_name("subject", :facetable), limit: 5
@@ -53,7 +54,11 @@ class CatalogController < ApplicationController
     config.add_facet_field solr_name("file_format", :facetable), limit: 5
     config.add_facet_field solr_name('member_of_collection_ids', :symbol), limit: 5, label: 'Collections', helper_method: :collection_title_by_id
     config.add_facet_field solr_name("degree", :facetable), label: "Degree", limit: 5
-    config.add_facet_field solr_name("department", :facetable), label: "Department", limit: 5
+    config.add_facet_field solr_name("department", :facetable), label: "Unit", limit: 5
+    config.add_facet_field solr_name("sponsor", :facetable), label: "Sponsor", limit: 5
+    config.add_facet_field solr_name("year", :facetable), label: "Year", limit: 5
+    config.add_facet_field solr_name("center", :facetable), label: "Center", limit: 5
+
 
     # The generic_type isn't displayed on the facet list
     # It's used to give a label to the filter that comes from the user profile
@@ -88,7 +93,7 @@ class CatalogController < ApplicationController
     # config.add_index_field solr_name("embargo_release_date", :stored_sortable, type: :date), label: "Embargo release date", helper_method: :human_readable_date
     # config.add_index_field solr_name("lease_expiration_date", :stored_sortable, type: :date), label: "Lease expiration date", helper_method: :human_readable_date
     config.add_index_field solr_name("degree", :stored_searchable), label: "Degree"
-    config.add_index_field solr_name("department", :stored_searchable), label: "Department"
+    config.add_index_field solr_name("department", :stored_searchable), label: "Unit"
 
     # solr fields to be displayed in the show (single result) view
     #   The ordering of the field names is the order of the display
@@ -100,7 +105,7 @@ class CatalogController < ApplicationController
     config.add_show_field solr_name("publisher", :stored_searchable)
     config.add_show_field solr_name("date_created", :stored_searchable)
     config.add_show_field solr_name("degree", :stored_searchable), label: "Degree"
-    config.add_show_field solr_name("department", :stored_searchable), label: "Department"
+    config.add_show_field solr_name("department", :stored_searchable), label: "Unit"
     config.add_show_field solr_name("format", :stored_searchable)
     config.add_show_field solr_name("identifier", :stored_searchable)
     config.add_show_field solr_name("subject", :stored_searchable)
