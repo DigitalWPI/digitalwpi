ae651b2c717f87110fcde0c56281795134f2d138
Alfred Scott <awscott@wpi.edu>
2019-05-23 11:11:20 -0400

    Added todo, get_new_works

--- app/models/eproject_record.rb
+++ app/models/eproject_record.rb
@@ -1,4 +1,14 @@
 class EprojectRecord < ApplicationRecord
+    def self.get_new_works(client_id,client_secret,oath_token_url,protected_url)
+        #todo
+        #get token using oath
+        # do a get of something?
+        # loop through responses
+        #   ingest works
+        #   create EprojectRecord associating work and eproject obj.
+        #   update eproj each time work ingested
+        #   update eproj each tiem work failed
+    end
     def work
         begin
             return ActiveFedora::Base.find(self.work_id)
