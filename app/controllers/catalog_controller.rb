1bf5a227bd928f38f5b7629e0d768186bf2c076d
Derek Murphy <dfmurphy@wpi.edu>
2020-04-08 13:10:56 -0400

    Updated Facet display and order

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -43,19 +43,16 @@ class CatalogController < ApplicationController
     #   The ordering of the field names is the order of the display
     # config.add_facet_field solr_name("human_readable_type", :facetable), label: "Type", limit: 5
     config.add_facet_field solr_name('member_of_collection_ids', :symbol), limit: 5, label: 'Collections', collapse: false, helper_method: :collection_title_by_id
-    config.add_facet_field solr_name("resource_type", :facetable), label: "Resource Type", limit: 5, collapse: false
-    config.add_facet_field solr_name("creator", :facetable), limit: 5
-    config.add_facet_field solr_name("contributor", :facetable), label: "Contributor", limit: 5
     config.add_facet_field solr_name("year", :facetable), label: "Year", limit: 5
-    config.add_facet_field solr_name("language", :facetable), limit: 5
-    config.add_facet_field solr_name("subject", :facetable), limit: 5
+    config.add_facet_field solr_name("creator", :facetable), limit: 5
     config.add_facet_field solr_name("advisor", :facetable), label: "Advisor", limit: 5
-    config.add_facet_field solr_name("department", :facetable), label: "Unit", limit: 5
-    config.add_facet_field solr_name("sponsor", :facetable), label: "Sponsor", limit: 5
-    config.add_facet_field solr_name("degree", :facetable), label: "Degree", limit: 5
-    config.add_facet_field solr_name("publisher", :facetable), limit: 5
+    config.add_facet_field solr_name("contributor", :facetable), label: "Contributor", limit: 5
     config.add_facet_field solr_name("center", :facetable), label: "Project Center", limit: 5
-    config.add_facet_field solr_name("based_near_label", :facetable), label: "Geo Location", limit: 5
+    config.add_facet_field solr_name("department", :facetable), label: "Unit", limit: 5
+    config.add_facet_field solr_name("publisher", :facetable), limit: 5
+    config.add_facet_field solr_name("subject", :facetable), limit: 5
+    config.add_facet_field solr_name("resource_type", :facetable), label: "Resource Type", limit: 5, collapse: false
+    #config.add_facet_field solr_name("language", :facetable), limit: 5
 
 
     # The generic_type isn't displayed on the facet list
