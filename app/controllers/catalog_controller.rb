9dfa4f4900087e52dbdf420affa0170f13b713fd
Derek Murphy <dfmurphy@wpi.edu>
2020-07-10 13:18:48 -0400

    testing index_range

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -43,12 +43,12 @@ class CatalogController < ApplicationController
     #   The ordering of the field names is the order of the display
     # config.add_facet_field solr_name("human_readable_type", :facetable), label: "Type", limit: 5
     config.add_facet_field solr_name('member_of_collection_ids', :symbol), limit: 5, label: 'Collections', collapse: false, helper_method: :collection_title_by_id
-    config.add_facet_field solr_name("year", :facetable), label: "Year", limit: 5
-    config.add_facet_field solr_name("creator", :facetable), limit: 5
-    config.add_facet_field solr_name("advisor", :facetable), label: "Advisor", limit: 5
-    config.add_facet_field solr_name("contributor", :facetable), label: "Contributor", limit: 5
-    config.add_facet_field solr_name("center", :facetable), label: "Project Center", limit: 5
-    config.add_facet_field solr_name("department", :facetable), label: "Unit", limit: 5
+    config.add_facet_field solr_name("year", :facetable), label: "Year", limit: 5, sort: 'index', index_range: 'A'..'Z'
+    config.add_facet_field solr_name("creator", :facetable), limit: 5, sort: 'index', index_range: 'A'..'Z'
+    config.add_facet_field solr_name("advisor", :facetable), label: "Advisor", limit: 5, sort: 'index', index_range: 'A'..'Z'
+    config.add_facet_field solr_name("contributor", :facetable), label: "Contributor", limit: 5, sort: 'index', index_range: 'A'..'Z'
+    config.add_facet_field solr_name("center", :facetable), label: "Project Center", limit: 5, sort: 'index', index_range: 'A'..'Z'
+    config.add_facet_field solr_name("department", :facetable), label: "Unit", limit: 5, sort: 'index', index_range: 'A'..'Z'
     config.add_facet_field solr_name("publisher", :facetable), limit: 5
     config.add_facet_field solr_name("subject", :facetable), limit: 5, sort: 'index', index_range: 'A'..'Z'
     config.add_facet_field solr_name("resource_type", :facetable), label: "Resource Type", limit: 5
