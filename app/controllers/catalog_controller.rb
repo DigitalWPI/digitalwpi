6bcc4af7fae7067c468da2b693f6d2b073ae8b3c
Derek Murphy <dfmurphy@wpi.edu>
2020-08-24 20:42:07 -0400

    changed the collection facet id

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -42,7 +42,8 @@ class CatalogController < ApplicationController
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
     # config.add_facet_field solr_name("human_readable_type", :facetable), label: "Type", limit: 5
-    config.add_facet_field solr_name('member_of_collection_ids', :symbol), limit: 5, label: 'Collections', collapse: false, helper_method: :collection_title_by_id
+    #config.add_facet_field solr_name('member_of_collection_ids', :symbol), limit: 5, label: 'Collections', collapse: false, helper_method: :collection_title_by_id
+    config.add_facet_field solr_name("member_of_collections", :symbol), limit: 5, label: 'Collections', collapse: false
     config.add_facet_field solr_name("year", :facetable), label: "Year", limit: 5, sort: 'index desc'
     config.add_facet_field solr_name("creator", :facetable), limit: 5, sort: 'index', index_range: 'A'..'Z'
     config.add_facet_field solr_name("advisor", :facetable), label: "Advisor", limit: 5, sort: 'index', index_range: 'A'..'Z'
