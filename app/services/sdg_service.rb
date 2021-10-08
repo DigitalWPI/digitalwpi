cdafec67b975bb31202180f578347a5601b0db89
Derek Murphy <dfmurphy@wpi.edu>
2021-10-08 12:23:21 -0400

    moved old_sdg variable

--- app/services/sdg_service.rb
+++ app/services/sdg_service.rb
@@ -2,18 +2,6 @@ module SdgService
 	mattr_accessor :authority
 	self.authority = Qa::Authorities::Local.subauthority_for('sdg')
 	
-	old_sdg = [
-	  "1 - No Poverty",
-	  "2 - Zero Hunger",
-	  "3 - Good Health and Well-being",
-	  "4 - Quality Education",
-	  "5 - Gender Equality",
-	  "6 - Clean Water and Sanitation",
-	  "7 - Affordable and Clean Energy",
-	  "8 - Decent Work and Economic Growth",
-	  "9 - Industry, Innovation and Infrastructure"
-	]
-	
 	def self.select_all_options
 		authority.all.map do |element|
 			[element[:label], element[:id]]
@@ -21,6 +9,18 @@ module SdgService
 	end
 	
 	def self.label(id)
+	  old_sdg = [
+	    "1 - No Poverty",
+	    "2 - Zero Hunger",
+	    "3 - Good Health and Well-being",
+	    "4 - Quality Education",
+	    "5 - Gender Equality",
+	    "6 - Clean Water and Sanitation",
+	    "7 - Affordable and Clean Energy",
+	    "8 - Decent Work and Economic Growth",
+	    "9 - Industry, Innovation and Infrastructure"
+	  ]
+
 	  if old_sdg.include(id)
 	    id
 	  else
