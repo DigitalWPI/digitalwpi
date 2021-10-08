c91998809d58458898ffbf69b1e65476c38e24cc
Derek Murphy <dfmurphy@wpi.edu>
2021-10-08 12:18:10 -0400

    updated the SDG label to cut back on errors

--- app/services/sdg_service.rb
+++ app/services/sdg_service.rb
@@ -2,6 +2,18 @@ module SdgService
 	mattr_accessor :authority
 	self.authority = Qa::Authorities::Local.subauthority_for('sdg')
 	
+	old_sdg = [
+	  "1 - No Poverty",
+	  "2 - Zero Hunger",
+	  "3 - Good Health and Well-being",
+	  "4 - Quality Education",
+	  "5 - Gender Equality",
+	  "6 - Clean Water and Sanitation",
+	  "7 - Affordable and Clean Energy",
+	  "8 - Decent Work and Economic Growth",
+	  "9 - Industry, Innovation and Infrastructure"
+	]
+	
 	def self.select_all_options
 		authority.all.map do |element|
 			[element[:label], element[:id]]
@@ -9,6 +21,10 @@ module SdgService
 	end
 	
 	def self.label(id)
-		authority.find(id).fetch('term')
+	  if old_sdg.include(id)
+	    id
+	  else
+		  authority.find(id).fetch('term')
+		end
 	end
 end
