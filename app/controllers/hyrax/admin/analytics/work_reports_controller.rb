cfa93109e5024103c3443540ba0ef06795296d15
gyangupta10 <gyan@cottagelabs.com>
2025-08-28 17:30:30 +0530

    fixes for work reports page not loading

--- app/controllers/hyrax/admin/analytics/work_reports_controller.rb
+++ app/controllers/hyrax/admin/analytics/work_reports_controller.rb
@@ -8,42 +8,40 @@ module Hyrax
         def index
           return unless Hyrax.config.analytics? && Hyrax.config.analytics_provider != 'ga4'
 
-          @accessible_works ||= accessible_works
-          @project_centers = project_centers
+          @start_date, @end_date = date_range.split(',').map(&:to_date)
+          @accessible_works = accessible_works
+          @accessible_file_sets = accessible_file_sets
 
-          if params['center_tesim'].present?
-            @accessible_works = @accessible_works.select{|work| work['center_tesim']&.include?(params['center_tesim'])}
-          end
-
-          if params['member_of_collection_ids_ssim'].present?
-            @accessible_works = @accessible_works.select{|work| work['member_of_collection_ids_ssim']&.include?(params['member_of_collection_ids_ssim'])}
-          end
-
-          @accessible_file_sets ||= accessible_file_sets
-          @works_count = @accessible_works.count
-          @top_works = paginate(top_works_list, rows: 10)
-          @top_file_set_downloads = paginate(top_files_list, rows: 10)
-
-          if current_user.ability.admin?
-            @pageviews = Hyrax::Analytics::Results.new(WorkViewStat.all.map{|stat| [stat.date, stat.work_views]})
-            @downloads = Hyrax::Analytics::Results.new(FileDownloadStat.all.map{|stat| [stat.date, stat.downloads]})
-          end
-
+          work_views = precompute_work_views
+          file_downloads = precompute_file_downloads
+          work_file_downloads = precompute_work_downloads(file_downloads)
+          
+          @works_count = work_views.size
+          
           respond_to do |format|
-            format.html
-            format.csv { export_data }
+            format.html do
+              @project_centers = project_centers
+              @top_works = paginate(build_top_works_list(work_views, work_file_downloads), rows: 10)
+              @top_file_set_downloads = paginate(build_top_files_list(file_downloads), rows: 10)
+    
+              if current_user.ability.admin?
+                @pageviews = Hyrax::Analytics::Results.new(work_stats.pluck(:date, :work_views))
+                @downloads = Hyrax::Analytics::Results.new(file_stats.pluck(:date, :downloads))
+              end
+            end
+            format.csv { export_data(work_views, work_file_downloads) }
           end
         end
 
         def show
-          @pageviews =  Hyrax::Analytics::Results.new(WorkViewStat.where(work_id: @document.id).map{|stat| [stat.date, stat.work_views]})
+          @pageviews =  Hyrax::Analytics::Results.new(WorkViewStat.where(work_id: @document.id).where("work_views > 0").map{|stat| [stat.date, stat.work_views]})
           @uniques = Hyrax::Analytics.unique_visitors_for_id(@document.id)
-          @file_download_stats = FileDownloadStat.where(file_id: @document._source["file_set_ids_ssim"])
+          @file_download_stats = FileDownloadStat.where(file_id: @document._source["file_set_ids_ssim"]).where("downloads > 0")
           @downloads = Hyrax::Analytics::Results.new(@file_download_stats.map{|stat| [stat.date, stat.downloads]})
           @files = paginate(@document._source["file_set_ids_ssim"], rows: 5)
           respond_to do |format|
             format.html
-            format.csv { export_data }
+            format.csv { export_work_data }
           end
         end
 
@@ -51,14 +49,23 @@ module Hyrax
 
         def accessible_works
           models = Hyrax.config.curation_concerns.map { |m| "\"#{m}\"" }
+          query = "has_model_ssim:(#{models.join(' OR ')})"
+          
+          if params['center_tesim'].present?
+            query += " AND center_tesim:\"#{params['center_tesim']}\""
+          end
+          
+          if params['member_of_collection_ids_ssim'].present?
+            query += " AND member_of_collection_ids_ssim:#{params['member_of_collection_ids_ssim']}"
+          end
+
+          fl = 'id, title_tesim, member_of_collection_ids_ssim, file_set_ids_ssim, center_tesim'
           if current_user.ability.admin?
-            ActiveFedora::SolrService.query("has_model_ssim:(#{models.join(' OR ')})",
-              fl: 'title_tesim, id, member_of_collection_ids_ssim, center_tesim',
-              rows: 50_000)
+            ActiveFedora::SolrService.query(query, fl: fl, rows: 50_000)
           else
             ActiveFedora::SolrService.query(
-              "edit_access_person_ssim:#{current_user} AND has_model_ssim:(#{models.join(' OR ')})",
-              fl: 'title_tesim, id, member_of_collection_ids_ssim, center_tesim',
+              "#{query} AND edit_access_person_ssim:#{current_user}",
+              fl: fl,
               rows: 50_000
             )
           end
@@ -80,63 +87,104 @@ module Hyrax
           end
         end
 
-        def top_analytics_works
-          start_date, end_date = date_range.split(',').map(&:to_date)
-          @top_analytics_works ||= WorkViewStat.where(date: start_date..end_date).group(:work_id)
-          .select(:work_id, 'SUM(work_views) AS total_views')
-          .order('total_views DESC')
-        end
-
-        def top_analytics_downloads(work_id)
-          document = ::SolrDocument.find(work_id)
-          start_date, end_date = date_range.split(',').map(&:to_date)
-          FileDownloadStat.where(date: start_date..end_date, file_id: document._source["file_set_ids_ssim"])
-        end
-
-        def top_analytics_file_sets
-          start_date, end_date = date_range.split(',').map(&:to_date)
-          @top_analytics_file_sets ||= FileDownloadStat.where(date: start_date..end_date).group(:file_id)
-          .select(:file_id, 'SUM(downloads) AS total_downloads')
-          .order('total_downloads DESC')
-        end
-
-        def top_works_list
-          list = []
-          top_analytics_works
-          @accessible_works.each do |doc|
-            views_match = @top_analytics_works.detect { |stat| stat.work_id == doc["id"] }
-            total_views = views_match ? views_match.total_views : 0
-            downloads_match = top_analytics_downloads( doc["id"])
-            @download_count = downloads_match ? downloads_match.sum(:downloads) : 0
-            list.push([doc["id"], doc["title_tesim"].join(''), total_views, @download_count, doc["member_of_collections"]])
+        def precompute_work_views
+          work_stats.group_by(&:work_id).transform_values do |rows|
+            rows.sum(&:work_views)
           end
-          list.sort_by { |l| -l[2] }
         end
 
-        def top_files_list
-          list = []
-          top_analytics_file_sets
-          @accessible_file_sets.each do |doc|
-            downloads_match = @top_analytics_file_sets.detect { |stat| stat.file_id == doc["id"] }
-            @download_count = downloads_match ? downloads_match.total_downloads : 0
-            list.push([doc["id"], doc["title_tesim"].join(''), @download_count]) if doc["title_tesim"].present?
+        def precompute_file_downloads
+          file_stats.group_by(&:file_id).transform_values do |rows|
+            rows.sum(&:downloads)
           end
-          list.sort_by { |l| -l[2] }
         end
 
-        def export_data
-          data = top_works_list
-          csv_row = CSV.generate do |csv|
-            csv << ["Name", "ID", "Work Page Views", "Total Downloads of File Sets In Work", "Collections"]
-            data.each do |d|
-              csv << [d[0], d[1], d[2], d[3], d[4]]
+        def precompute_work_downloads(file_downloads)
+          # Build a hash mapping work_id to total downloads for its file sets, using only file_set_ids that have downloads
+          file_set_ids_with_downloads = file_downloads.keys.to_set
+          Hash.new(0).tap do |work_downloads|
+            @accessible_works.each do |work|
+              ids = Array(work['file_set_ids_ssim']) & file_set_ids_with_downloads.to_a
+              next if ids.empty?
+              sum = ids.sum { |fid| file_downloads[fid] || 0 }
+              work_downloads[work['id']] = sum
             end
           end
-          send_data csv_row, filename: "#{params[:start_date]}-#{params[:end_date]}-works.csv"
+        end
+
+        def build_top_works_list(work_views, work_downloads)
+          # Use a single pass and avoid filter_map for better memory efficiency
+          result = []
+          @accessible_works.each do |work|
+            id = work['id']
+            views = work_views[id]
+            next unless views
+            result << [
+              id,
+              work['title_tesim']&.join('') || '',
+              views,
+              work_downloads[id] || 0,
+              work['member_of_collection_ids_ssim']
+            ]
+          end
+          result.sort_by! { |work| -work[2] }
+          result
+        end
+
+        def build_top_files_list(file_downloads)
+          file_lookup = @accessible_file_sets.index_by { |f| f['id'] }
+          
+          file_downloads.filter_map do |id, count|
+            file = file_lookup[id]
+            next unless file
+            [
+              id,
+              file['title_tesim']&.join('') || '',
+              count
+            ]
+          end.sort_by { |file| -file[2] }
+        end
+
+        def export_data(work_views, work_downloads)
+          data = build_top_works_list(work_views, work_downloads)
+          csv = CSV.generate do |rows|
+            rows << ['Name', 'ID', 'Work Page Views', 'Total Downloads of File Sets In Work', 'Collections']
+            data.each do |row|
+              rows << [row[1], row[0], row[2], row[3], Array(row[4]).join('; ')]
+            end
+          end
+          send_data csv, filename: "#{params[:start_date]}-#{params[:end_date]}-works.csv"
+        end
+
+        def export_work_data
+          work = @document
+          file_set_ids = work._source["file_set_ids_ssim"] || []
+          file_sets = ActiveFedora::SolrService.query("{!terms f=id}#{file_set_ids.join(',')}", fl: 'id, title_tesim', rows: file_set_ids.size)
+          
+          file_downloads = FileDownloadStat.where(file_id: file_set_ids)
+                                          .group(:file_id)
+                                          .sum(:downloads)
+          
+          csv = CSV.generate do |rows|
+            rows << ['File Name', 'File ID', 'Downloads']
+            file_sets.each do |file|
+              rows << [file['title_tesim']&.join(''), file['id'], file_downloads[file['id']] || 0]
+            end
+          end
+          
+          send_data csv, filename: "work-#{work.id}-file-downloads.csv"
         end
 
         def project_centers
-          @accessible_works.map{|work| work['center_tesim']}.compact.flatten.uniq
+          @accessible_works.flat_map { |w| w['center_tesim'] }.compact.uniq
+        end
+
+        def work_stats
+          @work_stats ||= WorkViewStat.where(date: @start_date..@end_date).where("work_views > 0")
+        end
+
+        def file_stats
+          @file_stats ||= FileDownloadStat.where(date: @start_date..@end_date).where("downloads > 0")
         end
       end
     end
---
+++ lib/tasks/genrate_analytics_report.rake
@@ -0,0 +1,94 @@
+require 'json'
+
+namespace :wpi do
+  desc 'Generate analytics report Use: rake wpi:generate_analytics_report[start_date,end_date,member_of_collection_id,project_center]'
+  desc "Example: rake wpi:generate_analytics_report['2023-01-01','2023-12-31','js956f926','Project Center Name']"
+
+  task :generate_analytics_report, [:start_date, :end_date, :member_of_collection_id, :project_center] => :environment do |task, args|
+    @start_date = args[:start_date].present? ? args[:start_date].to_date : Hyrax.config.analytics_start_date.to_date
+    @end_date = args[:end_date].present? ? args[:end_date].to_date : Date.today
+    @center_tesim = args[:project_center]
+    @member_of_collection_ids_ssim = args[:member_of_collection_id]
+
+    @accessible_works = accessible_works
+    work_views = precompute_work_views
+    file_downloads = precompute_file_downloads
+    work_file_downloads = precompute_work_downloads(file_downloads)
+
+    data = build_top_works_list(work_views, work_file_downloads)
+    csv = CSV.open(Rails.root.join('tmp', "analytics_report_#{@start_date}_#{@end_date}.csv"), "wb") do |rows|
+      rows << ['Name', 'ID', 'Work Page Views', 'Total Downloads of File Sets In Work', 'Collections']
+      data.each do |row|
+        rows << [row[1], row[0], row[2], row[3], Array(row[4]).join('; ')]
+      end
+    end    
+  end
+
+  def build_top_works_list(work_views, work_downloads)
+    result = []
+    @accessible_works.each do |work|
+      id = work['id']
+      views = work_views[id]
+      next unless views
+      result << [
+        id,
+        work['title_tesim']&.join('') || '',
+        views,
+        work_downloads[id] || 0,
+        work['member_of_collection_ids_ssim']
+      ]
+    end
+    result.sort_by! { |work| -work[2] }
+    result
+  end
+
+  def work_stats
+    @work_stats ||= WorkViewStat.where(date: @start_date..@end_date).where("work_views > 0")
+  end
+
+  def file_stats
+    @file_stats ||= FileDownloadStat.where(date: @start_date..@end_date).where("downloads > 0")
+  end
+
+  def precompute_work_views
+    work_stats.group_by(&:work_id).transform_values do |rows|
+      rows.sum(&:work_views)
+    end
+  end
+
+  def precompute_file_downloads
+    file_stats.group_by(&:file_id).transform_values do |rows|
+      rows.sum(&:downloads)
+    end
+  end
+
+  def precompute_work_downloads(file_downloads)
+    # Build a hash mapping work_id to total downloads for its file sets, using only file_set_ids that have downloads
+    file_set_ids_with_downloads = file_downloads.keys.to_set
+    Hash.new(0).tap do |work_downloads|
+      @accessible_works.each do |work|
+        ids = Array(work['file_set_ids_ssim']) & file_set_ids_with_downloads.to_a
+        next if ids.empty?
+        sum = ids.sum { |fid| file_downloads[fid] || 0 }
+        work_downloads[work['id']] = sum
+      end
+    end
+  end
+
+  def accessible_works
+    models = Hyrax.config.curation_concerns.map { |m| "\"#{m}\"" }
+    query = "has_model_ssim:(#{models.join(' OR ')})"
+    
+    if @center_tesim.present?
+      query += " AND center_tesim:\"#{@center_tesim}\""
+    end
+
+    if @member_of_collection_ids_ssim.present?
+      query += " AND member_of_collection_ids_ssim:#{@member_of_collection_ids_ssim}"
+    end
+
+    fl = 'id, title_tesim, member_of_collection_ids_ssim, file_set_ids_ssim, center_tesim'
+    
+    ActiveFedora::SolrService.query(query, fl: fl, rows: 50_000)
+  end
+end
