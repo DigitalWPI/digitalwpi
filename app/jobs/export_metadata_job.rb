19dfe30fded5c2e7770c3a4a0afd3b5322c446bc
Anusha Ranganathan <anusha@cottagelabs.com>
2026-09-04 12:28:50 +0530

    Add map ids to job

--- app/jobs/export_metadata_job.rb
+++ app/jobs/export_metadata_job.rb
@@ -2,5 +2,6 @@ class ExportMetadataJob < Hyrax::ApplicationJob
   def perform(update=false, models_to_export=%w[Collection Etd GenericWork StudentWork FileSet User Role])
     ems = ExportMetadataService.new(update=update, models_to_export=models_to_export)
     ems.export
+    ems.map_ids
   end
 end
