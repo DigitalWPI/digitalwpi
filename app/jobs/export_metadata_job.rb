a14c652d853e75516b214f3e8d54eea184c1ac5e
Anusha Ranganathan <anusha@cottagelabs.com>
2026-09-17 18:17:20 +0530

    Update all exported files by default

--- app/jobs/export_metadata_job.rb
+++ app/jobs/export_metadata_job.rb
@@ -1,5 +1,5 @@
 class ExportMetadataJob < Hyrax::ApplicationJob
-  def perform(update=false, models_to_export=%w[Collection Etd GenericWork StudentWork FileSet User Role Id])
+  def perform(update=true, models_to_export=%w[Collection Etd GenericWork StudentWork FileSet User Role Id])
     ems = ExportMetadataService.new(update=update, models_to_export=models_to_export)
     ems.export
   end
