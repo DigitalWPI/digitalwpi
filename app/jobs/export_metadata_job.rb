7299a6a514c6a67fd63940bfbc75a5f8a9893ece
Anusha Ranganathan <anusha@cottagelabs.com>
2026-09-17 20:04:18 +0530

    Add option to set the base directory

--- app/jobs/export_metadata_job.rb
+++ app/jobs/export_metadata_job.rb
@@ -1,6 +1,6 @@
 class ExportMetadataJob < Hyrax::ApplicationJob
-  def perform(update=true, models_to_export=%w[Collection Etd GenericWork StudentWork FileSet User Role Id])
-    ems = ExportMetadataService.new(update=update, models_to_export=models_to_export)
+  def perform(update=true, models_to_export=%w[Collection Etd GenericWork StudentWork FileSet User Role Id], base_dir="/tmp/metadata_export")
+    ems = ExportMetadataService.new(update=update, models_to_export=models_to_export, base_dir=base_dir)
     ems.export
   end
 end
